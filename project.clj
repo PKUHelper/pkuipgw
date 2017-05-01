fe602c892cf047ab8b95dce2b8bc4190a814c387
Luolc <luolc.witty@gmail.com>
2017-05-01 22:29:50 +0800

    release: 1.0.1-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject pkuipgw "1.0.0"
+(defproject pkuipgw "1.0.1-SNAPSHOT"
   :description "PKU IP Gateway connection handler."
   :url "https://github.com/PKUHelper/pkuipgw"
   :license {:name "Eclipse Public License"
